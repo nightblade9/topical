963e89d18d16cc474e0e9c603e27584e68c9be0e
nightblade9 <nightbladecodes@gmail.com>
2021-01-04 16:22:52 -0500

    Script fundamentals: can run and print args

---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import sys
+from topical.topical import Topical
+
+Topical().run(sys.argv)
---
+++ topical/topical.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+class Topical:
+    def __init__(self):
+        pass
+
+    def run(self, args):
+        print("Hello, topical! {}".format(args))
